f19eab4691dd80b896148e26abbb6df7b7258f86
timadewale1 <timadewale1@gmail.com>
2023-09-03 19:36:42 +0100

    commit

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,104 +1,128 @@
-import React, { useState, useRef, useEffect } from 'react' 
-import { BsExclamationLg, BsPatchCheckFill } from 'react-icons/bs';
-import Fade from 'react-reveal'
-import Reveal from 'react-reveal/Reveal'
-
+import React, { useState, useRef } from 'react'
+import { BsExclamationLg, BsPatchCheckFill } from 'react-icons/bs'
+import { RiSendPlane2Fill } from 'react-icons/ri'
 import Blast from '../../components/BlastAnimation/Blast'
 import Map from '../../components/map/Map'
+import Fade from 'react-reveal/Fade'
+import Reveal from 'react-reveal/Reveal'
 
-import emailjs from "@emailjs/browser"
 
+// Email js
+import emailjs from '@emailjs/browser'
 import './contact.scss'
 
-const Contact = () => {
-const [letterClass, setLetterClass] = useState("text-animate")
-useEffect(() => {
-    setTimeout(() => {
-        setLetterClass("text-animate-hover")
-    },3000)
-}, [])
+const Contact = React.memo(() => {
+  const [letterClass, setLetterClass] = React.useState('text-animate')
+  React.useEffect(() => {
+    const interval = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
 
-    const [validInpt, setValidInpt] = useState({
-        name: '',
-        message: '',
-        email: '',
-        subject: '', 
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
+  const [validInpt, setValidInpt] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setValidInpt((preValue) => {
+      return { ...preValue, [name]: value }
     })
+  }
 
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setValidInpt((preValue) => {
-    return { ...preValue, [name]: value }
-        })
-}
+  // select all the input with useRef Hook
+  const emailRef = useRef(null)
+  const TextAreaRef = useRef(null)
+  const MessageRef = useRef(null)
 
+  const form = useRef(null)
 
-    const emailRef = useRef(null)
-    const TextAreaRef = useRef(null)
-    const MessageRef = useRef(null)
-    const form = useRef(null)
+  // show Message function
+  const showMessage = (message, updateColor) => {
+    const divContent = document.createElement('div')
+    divContent.textContent = message
+    divContent.classList.add('div-content')
+    MessageRef.current.prepend(divContent)
+    divContent.style.backgroundColor = updateColor
 
-    const setError = (inputRef) => {
-        if (inputRef.current.parentElement.classList.contains("success")) {
-            inputRef.current.parentElement.classList.remove("success")
-        }
-        else {
-            inputRef.current.parentElement.classList.add("error")
-        }
+    MessageRef.current.style.transform = `translateX(${'0'}%)`
+    MessageRef.current.style.visibility = 'visible'
+    setTimeout(() => {
+      divContent.classList.add('hide')
+      divContent.addEventListener('transitionend', () => {
+        divContent.remove()
+      })
+      divContent.style.transform = `translateX(${'0'}%)`
+      // MessageRef.current.style.visibility = 'visible'
+      emailRef.current.parentElement.classList.remove('error')
+      TextAreaRef.current.parentElement.classList.remove('error')
+      emailRef.current.parentElement.classList.remove('success')
+      TextAreaRef.current.parentElement.classList.remove('success')
+    }, 5000)
+  }
+  // Error function
+  const setError = (inputRef) => {
+    if (inputRef.current.parentElement.classList.contains('success')) {
+      inputRef.current.parentElement.classList.remove('success')
+    } else {
+      inputRef.current.parentElement.classList.add('error')
     }
+  }
 
-    const setSuccess = (inputRef) => {
-        if (inputRef.current.parentElement.classList.contains("error")) {
-            inputRef.current.parentElement.classList.remove("error")
-        }
-        else {
-            inputRef.current.parentElement.classList.add("success")
-        }
+  // success Function
+  const setSuccess = (inputRef) => {
+    if (inputRef.current.parentElement.classList.contains('error')) {
+      inputRef.current.parentElement.classList.remove('error')
+    } else {
+      inputRef.current.parentElement.classList.add('success')
     }
+  }
 
-    const showMessage = (message) => {
-        const divContent = document.createElement("div")
-        divContent.textContent = message
-        divContent.classList.add("div-content")
-        MessageRef.current.prepend(divContent)
+  const onSubmit = (e) => {
+    e.preventDefault()
+    const { email, message } = validInpt
+    // const pattern = /^[^]+@[^]+\.[a-z]{2,3}$/
+    const pattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+    if (!email && !message) {
+      setError(emailRef)
+      setError(TextAreaRef)
+      showMessage('Pls! fill in the required inputs')
+    } else if (!email && message) {
+      setError(emailRef)
 
-        MessageRef.current.style.transform = `translatex(${0}%)`
+      showMessage("Ooops! Email can't be empty")
+    } else if (!email.match(pattern)) {
+      setError(emailRef)
+      showMessage('Ooops! Email not valid')
+    } else if (!message && email.match(pattern)) {
+      setError(TextAreaRef)
 
-        setTimeout(() => {
-            divContent.classList.add('hide')
+      showMessage('Leave a message pls!')
+    } else if (email && !message) {
+      setError(TextAreaRef)
+      showMessage('por favor! enviar un mensaje')
+      
+    } else if (email && message) {
+             emailjs.send('service_i2egx9v', "template_779e39k", form.current, "27nO3py507aQHesaK");
+      setSuccess(emailRef)
+      setSuccess(TextAreaRef)
+      showMessage('Message sent successfully', 'green')
 
-            divContent.addEventListener("transitionend", () => {
-                divContent.remove()
-            })
-
-            emailRef.current.parentElement.classList.remove('error')
-            emailRef.current.parentElement.classList.remove('success')
-            TextAreaRef.current.parentElement.classList.remove('error')
-            TextAreaRef.current.parentElement.classList.remove('success')
-        }, 3000)
+      setValidInpt({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+      })
     }
-    const onSubmit = (e) => {
-        e.preventDefault()
-        const { email, message } = validInpt
-        if (!email && !message) {
-            setError(emailRef)
-            setError(TextAreaRef)
-            showMessage("Please! fill in the required input")
-        }
-        else if (email && message) {
-
-            emailjs.send('service_i2egx9v', "template_779e39k", form.current, "27nO3py507aQHesaK");
-            setSuccess(emailRef)
-            setSuccess(TextAreaRef)
-            showMessage("Message sent succesfully")
-            setValidInpt({
-                name: '',
-                message: '',
-                subject: '',
-                email: '',
-            })
-        }
-    }
+  }
     return (
         <>
             <section className='contact-section'>
@@ -114,91 +138,98 @@ useEffect(() => {
                         </h2>
                         <Fade bottom>
                             <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit consequuntur veritatis in optio quos molestiae suscipit fugiat? Sint, fuga laborum nesciunt commodi deleniti cum et sit pariatur necessitatibus eveniet fugit!
+                                I'm available for freelancing, hit me up if you have any project to work on.
                             </p>
                         </Fade>
                     </div>
                     <Fade bottom>
-                        <div className='input-wrapper'>
-                            <div className='form-input-group'>
-                                <input 
-                                type="text" 
-                                name='name' 
-                                placeholder='Name' 
-                                value={validInpt.name} 
-                                autoComplete="false" 
-                                onChange={handleChange}
-                                />
-                                <span className='switch__bg'></span>
-                            </div>
-                             <div className='form-input-group'>
-                                <input 
-                                type="text" 
-                                name='email' 
-                                placeholder='Email' 
-                                value={validInpt.email} 
-                                autoComplete="false" 
-                                onChange={handleChange}
-                                ref={emailRef}
-                                />
-                                <span className='switch__bg'></span>
-                                <BsExclamationLg  className="exclamation" />
-                                <BsPatchCheckFill className="checkCircle" />
-                            </div>
-                        </div>
-                    </Fade>
-                    <Fade bottom>
-                         <div className='form-input-group'>
-                                <input 
-                                type="text" 
-                                name='subject' 
-                                placeholder='Subject' 
-                                value={validInpt.subject} 
-                                autoComplete="false" 
-                                onChange={handleChange}
-                                />
-                                <span className='switch__bg'></span>
-                            </div>
-                    </Fade>
-                    <Fade bottom>
-                         <div className='form-input-group'>
-                            <textarea
-                                autoComplete='false'
-                                ref={TextAreaRef}
-                                type="text"
-                                placeholder='Message'
-                                name='Message'
-                                value={validInpt.message}
-                                onChange={handleChange}>
-                            </textarea>
-                            <span className='switch__bg'></span>
-                            <BsExclamationLg  className="exclamation" />
-                            <BsPatchCheckFill className="checkCircle" />
-                            </div>
-                    </Fade>
-                    <Fade left>
-                        <button className='contact-button submit-button' type='submit'>
-                            <div>
-                                <span className='bg switch__bg'></span>
-                                <span className='base switch__border__color'></span>
-                                <span className='text'>Send message</span>
-                            </div>
-                        </button>
-                    </Fade>
-                    <Reveal bottom>
-                        <footer className='footer'>
-                            <p>&copy; copyright All right Reserved ADE.T - 2023</p>
-                            </footer>
-                    </Reveal>
-                </form>
-                  <div className='google-map'>
-                        <Map />
-                </div>
-                <div className='message' ref={MessageRef}>
-                </div>
-            </section>
-        </>
-    ) 
-}
+            <div className="input-wrapper">
+              <div className="form-input-group">
+                <input
+                  autoComplete="false"
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                  value={validInpt.name}
+                  onChange={handleChange}
+                />
+                <span className="switch__bg"></span>
+                <BsExclamationLg className="exclamation" />
+                <BsPatchCheckFill className="checkCircle" />
+              </div>
+
+              <div className="form-input-group">
+                <input
+                  autoComplete="false"
+                  ref={emailRef}
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={validInpt.email}
+                  onChange={handleChange}
+                />
+                <span className="switch__bg"></span>
+                <BsExclamationLg className="exclamation" />
+                <BsPatchCheckFill className="checkCircle" />
+              </div>
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="form-input-group">
+              <input
+                autoComplete="false"
+                type="text"
+                placeholder="Subject"
+                name="subject"
+                value={validInpt.subject}
+                onChange={handleChange}
+              />
+              <span className="switch__bg"></span>
+              <BsExclamationLg className="exclamation" />
+              <BsPatchCheckFill className="checkCircle" />
+            </div>
+          </Fade>
+          <Fade bottom>
+            <div className="form-input-group">
+              <textarea
+                autoComplete="false"
+                ref={TextAreaRef}
+                type="text"
+                placeholder="Message"
+                name="message"
+                value={validInpt.message}
+                onChange={handleChange}
+              />
+              <span className="switch__bg"></span>
+              <BsExclamationLg className="exclamation" />
+              <BsPatchCheckFill className="checkCircle" />
+            </div>
+          </Fade>
+          <Fade left>
+            <button type="submit" className="contact-button submit-button">
+              <div>
+                <span className="bg switch__bg"></span>
+                <span className="base switch__border-color"></span>
+                <span className="text">
+                  Send Message
+                  <RiSendPlane2Fill className="message-deliver" />
+                </span>
+              </div>
+            </button>
+          </Fade>
+          <Reveal bottom>
+            <footer className="footer">
+              <p> &copy; copyright All Right Reserved Almarex - 2022</p>
+            </footer>
+          </Reveal>
+        </form>
+        <div className="google-map">
+          <Map />
+        </div>
+        <div ref={MessageRef} className="message"></div>
+      </section>
+    </>
+  )
+})
 
 export default Contact
