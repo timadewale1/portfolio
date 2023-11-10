c9351e0ef4b7ae3ebafc7b207cadeb57c316d9f4
timadewale1 <timadewale1@gmail.com>
2023-11-10 13:02:48 +0100

    commit

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import { GrGithub } from 'react-icons/gr'
-import { TbWorld } from 'react-icons/tb'
+import { GrGithub } from "react-icons/gr";
+import { TbWorld } from "react-icons/tb";
 
 // images from the asset folder in the image file
 import {
@@ -11,88 +11,90 @@ import {
   trade,
   resturant,
   pharmacy,
-} from '../../assets/images'
+} from "../../assets/images";
 
 // projects' id, name, stacks, icon and url data
 export const projects = [
   {
     id: 1,
-    name: 'Trade Activity',
-    desc:
-      'A landing page for a business website',
-    stack: 'HTML CSS JAVASCRIPT',
+    name: "Trade Activity",
+    desc: "A landing page for a business website",
+    stack: "HTML CSS JAVASCRIPT",
     gitHubUrl: {
       icon: <GrGithub />,
-      url: 'https://github.com/timadewale1/tradeactivity',
+      url: "https://github.com/timadewale1/tradeactivity",
     },
     website: {
       icon: <TbWorld />,
-      url: 'https://tradeactivity.vercel.app/',
+      url: "https://tradeactivity.vercel.app/",
     },
     imgUrl: trade,
   },
   {
     id: 2,
-    name: 'Restaurant',
-    desc: 'A Responsive restaurant website design.',
-    stack: 'HTML CSS JAVASCRIPT',
+    name: "Restaurant",
+    desc: "A Responsive restaurant website design.",
+    stack: "HTML CSS JAVASCRIPT",
     gitHubUrl: {
       icon: <GrGithub />,
-      url: 'https://github.com/timadewale1/restaurant',
+      url: "https://github.com/timadewale1/restaurant",
     },
     website: {
       icon: <TbWorld />,
-      url: 'https://timadewale1.github.io/restaurant/',
+      url: "https://timadewale1.github.io/restaurant/",
     },
     imgUrl: resturant,
   },
   {
     id: 3,
-    name: 'first portfolio',
-    desc: 'This is my first official portfolio. A copy of Jacek Jeznach portfolio',
-    stack: 'HTML CSS JAVASCRIPT',
+    name: "first portfolio",
+    desc:
+      "This is my first official portfolio. A copy of Jacek Jeznach portfolio",
+    stack: "HTML CSS JAVASCRIPT",
     imgUrl: main,
 
     gitHubUrl: {
       icon: <GrGithub />,
-      url: 'https://github.com/timadewale1/official-portfolio',
+      url: "https://github.com/timadewale1/official-portfolio",
     },
     website: {
       icon: <TbWorld />,
-      url: 'https://adet-portfolio.vercel.app/',
+      url: "https://adet-portfolio.vercel.app/",
     },
   },
   {
     id: 4,
-    name: 'Pharmacy landing page',
-    desc: 'A Static Pharmacy landing Page',
-    stack: 'HTML CSS',
+    name: "Pharmacy landing page",
+    desc: "A Static Pharmacy landing Page",
+    stack: "HTML CSS",
     gitHubUrl: {
       icon: <GrGithub />,
-      url: 'https://github.com/timadewale1/official-portfolio/tree/main/Create_jack-Portfolio/pharmacy',
+      url:
+        "https://github.com/timadewale1/official-portfolio/tree/main/Create_jack-Portfolio/pharmacy",
     },
     website: {
       icon: <TbWorld />,
-      url: 'https://adet-portfolio.vercel.app/pharmacy/pharmacy.html',
+      url: "https://adet-portfolio.vercel.app/pharmacy/pharmacy.html",
     },
     imgUrl: pharmacy,
   },
 
-  // {
-  //   id: 5,
-  //   name: 'Rest Country API',
-  //   desc: 'A project that displays all information about a country',
-  //   stack: 'React JS',
-  //   gitHubUrl: {
-  //     icon: <GrGithub />,
-  //     url: 'https://github.com/Almarex-Web-Dev/rest-countries-api-project',
-  //   },
-  //   website: {
-  //     icon: <TbWorld />,
-  //     url: 'https://rest-countries-web-app-project.netlify.app/',
-  //   },
-  //   imgUrl: restCOuntry,
-  // },
+  {
+    id: 5,
+    name: "Area Calculator App",
+    desc:
+      "An app to calculate Area of different shapes with different units of measurement.",
+    stack: "next JS",
+    gitHubUrl: {
+      icon: <GrGithub />,
+      url: "https://github.com/timadewale1/Area-Calculator",
+    },
+    website: {
+      icon: <TbWorld />,
+      url: "https://timadewale1.github.io/Area-Calculator/",
+    },
+    imgUrl: [],
+  },
   // {
   //   id: 6,
   //   name: 'Huddle Landing Page',
@@ -139,4 +141,4 @@ export const projects = [
   //   },
   //   imgUrl: gericht,
   // },
-]
+];
